cccb720e1918641097ca714201649181a854815e
SSKGo <54466390+SSKGo@users.noreply.github.com>
2024-03-24 20:48:05 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 # coding: utf-8
 import os
 import re
+import shutil
 import sys
 
 from py2exe import freeze
 
-# Create license information
-dir_file = os.path.dirname(__file__)
+dir_root = os.path.dirname(__file__)
+dir_source = os.path.join(dir_root, "source")
+dir_dist = os.path.join(dir_root, "dist")
 
-with open(os.path.join(dir_file, "OSS_LICENSES"), "r", encoding="utf-8") as f:
+# Create 3rd-party python library license information
+with open(os.path.join(dir_root, "OSS_LICENSES"), "r", encoding="utf-8") as f:
     licenses = f.read()
 
+# Constract python license information
 with open(
     os.path.join(sys.base_prefix, "LICENSE_PYTHON.txt"), "r", encoding="utf-8"
 ) as f:
     python_license = f.read()
-
 python_copyright = re.search(
     r"Copyright\s\(c\)[\s\S]*?All\sRights\sReserved", python_license
 ).group()
-
 licenses += f"""python
 {sys.version.split("|")[0].strip()}
 {python_copyright}
@@ -27,24 +29,21 @@ licenses += f"""python
 {python_license}
 """
 
-
+# Inject license information into python source code
 description = f'oss_license_descriptions = """{licenses}"""'
-
-with open(
-    os.path.join(dir_file, "source", "oss_license_descriptions.py"),
-    "w",
-    encoding="utf-8",
-) as f:
+file_path = os.path.join(dir_source, "oss_license_descriptions.py")
+with open(file_path, "w", encoding="utf-8") as f:
     f.write(description)
 
 # Compile with py2exe
-dir_main = os.path.join(os.path.dirname(__file__), "source")
+# Delete dist before exe creation
+shutil.rmtree(dir_dist)
 
-sys.path.append(dir_main)
+sys.path.append(dir_source)
 sys.argv.append("py2exe")
 
 freeze(
     options={"py2exe": {"bundle_files": 1, "compressed": True}},
-    windows=[{"script": os.path.join(dir_main, "watchdog_s3_uploader.py")}],
+    windows=[{"script": os.path.join(dir_source, "app.py")}],
     zipfile=None,
 )
